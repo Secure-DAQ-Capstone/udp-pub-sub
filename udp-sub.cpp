d03b1b813804eba17494e49b1d353c7781f50a0e
Michael McCarthy <mjmccarthy@Michaels-MacBook-Pro.local>
2025-02-06 12:06:01 -0330

    initial commit

---
+++ udp-sub.cpp
@@ -0,0 +1,62 @@
+#ifndef UDP_SUB
+#define UDP_SUB
+
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+
+class UDPSub
+{
+    public:
+    UDPSub() = delete; // prevent usage of default constructor
+
+    UDPSub(size_t max_buffer_size, int port, in_addr_t address)
+    {
+        socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (socket_fd < 0) {
+            throw std::runtime_error("Error opening Socket");
+        }
+        max_buffer_size = max_buffer_size;
+        // Bind the socket to an address and port
+        sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+        server_addr.sin_addr.s_addr = address;
+        
+        if (bind(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            close(socket_fd);
+            throw std::runtime_error("Error binding Socket");
+        }
+
+    }
+
+    std::string read()
+    {
+        // Receive message
+        char buffer[max_buffer_size];
+        sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+
+        ssize_t recv_bytes = recvfrom(socket_fd, buffer, sizeof(buffer) - 1, 0, 
+                                    (sockaddr*)&client_addr, &client_len);
+        if (recv_bytes < 0) {
+            throw std::runtime_error("Error receiving message");
+        }
+        std::string received_message(buffer, recv_bytes);
+        std::cout << "Received message"<< std::endl;
+        return received_message;
+    }
+    ~UDPSub()
+    {
+        close(socket_fd);
+    }
+
+    private:
+        size_t max_buffer_size;
+        int socket_fd;
+};
+
+#endif // UDP_SUB
